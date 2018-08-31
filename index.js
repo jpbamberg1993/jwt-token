8edb00e29d63cefbc26e02d86412d22b2cae843c
John Paul Bamberg <jpbamberg1993@gmail.com>
2018-08-30 23:25:38 -0400

    Practice using jwt tokens

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const jwt = require("jsonwebtoken")
+const jwt = require('jsonwebtoken')
+const expressjwt = require('express-jwt')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 8888
 const users = [
   {id: 1, username: 'admin', password: 'admin'},
-  {id: 2, username: 'guest', password: 'thebambam'}
+  {id: 2, username: 'guest', password: 'thebambam'},
 ]
 
 app.use(bodyParser.json())
+app.use(cors())
+
+const jwtCheck = expressjwt({
+  secret: "mysupersecretkey"
+})
+
+app.get('/resource', (req, res) => {
+  res.status(200)
+     .send('Public resource, you can see this')
+})
+
+app.get(
+  '/resource/secret',
+  jwtCheck,
+  (req, res) => {
+    res.status(200)
+       .send('Secret resource, you should be logged in to see this!!!!')
+  })
 
 app.post('/login', (req, res) => {
   if (!req.body.username || !req.body.password) {
@@ -31,7 +51,7 @@ app.post('/login', (req, res) => {
   const token = jwt.sign({
     sub: user.id,
     username: user.username
-  }, "mysupersecretkey", {expiresIn: "3 hours"})
+  }, 'mysupersecretkey', {expiresIn: '3 hours'})
 
   res.status(200)
      .send({access_token: token})
